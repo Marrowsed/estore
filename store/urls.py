a4ae450c536923590878d7b9464fbad1656d47ac
Marrowsed <marcus.ah@gmail.com>
2022-09-21 16:20:39 -0300

    checkout urls

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,12 @@ from .views import *
 
 urlpatterns = [
     path('', ViewStore.as_view(), name="store-view"),
-    path('<int:pk>/add', cart_add, name='cart-add'),
+    path('<int:pk>/add', cart_add_index, name='cart-add'),
     path('<int:pk>', ProductDetail.as_view(), name='product-detail'),
     path('cart', ViewCart.as_view(), name='cart-detail'),
+    path('cart/<int:pk>/add', cart_plus, name='cart-plus'),
+    path('cart/<int:pk>/delete', cart_minus, name='cart-minus'),
+    path('checkout', ViewCheckout.as_view(), name='checkout-detail'),
+
 
 ]
